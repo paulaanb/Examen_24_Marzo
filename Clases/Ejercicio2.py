796ce29bb9ff9c68a75d2764ec4433d67fed18cc
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-03-24 09:35:41 +0100

    Correccion ejercicio 2

--- Clases/Ejercicio2.py
+++ Clases/Ejercicio2.py
@@ -3,9 +3,10 @@
 #Describa las relaciones entre los diferentes objetos. El diagrama asociado se llama diagrama de clases.
 #Dar un ejemplo de descripción algorítmica de las clases asociadas (únicamente la declaración de clase XXX).
 #¿Es posible implementar estos objetos en Python?
-import Animal from clases
-import Mamífero from clases
-import Ovíparo from clases
-import Pollo from clases
-import Gato from clases
-import Ornitorrinco from clases
+
+#Definimos la clase a utilizar
+class Animal:
+    def __init__(self, nombre, especie):
+        self.nombre = nombre
+        self.especie = especie
+
