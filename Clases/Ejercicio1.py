ac3830d3d779dc57730f8dfcb9732df25b9fee10
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-03-24 10:07:32 +0100

    definimos la variable

--- Clases/Ejercicio1.py
+++ Clases/Ejercicio1.py
@@ -28,4 +28,15 @@ class libro:
 
     def mostrarlibro(self):
         print("\nNombre: " + str(self.nombre()) + "\nAutor: " + self.autor() + "\nGenero: " + self.genero() + "\nAño de publicacion: " + str(self.añodepublicacion()) + "\nTitulo del libro: " + self.titulo() + "\nISBN: " + str(self.ISBN()) + "\nNumero de edicion: " + str(self.edicion()))
-        
+
+#Definimos las variables:
+nombre= raw_input("Por favor, escriba el nombre del libro: ")
+autor = raw_input("Por favor, introduzca el nombre del autor del libro: ")
+genero = raw_input("Por favor, introduzca el genero al que pertenece el libro: ")
+añodepublicacion = input("Por favor, introduzca el año de publicacion del libro: ")
+titulo = raw_input("Por favor, introduca el titulo del libro: ")
+ISBN = input("Por favor, introduzca el ISBN del libro: ")
+edicion = input("Por favor, introduzca el numero de edicion del libro: ")
+
+paula = libro(nombre, autor, genero, añodepublicacion, titulo, ISBN, edicion)
+paula.mostrarlibro()
