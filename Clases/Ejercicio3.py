d42f048df2b557c9a91a92729995060c274026ae
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-03-24 09:17:30 +0100

    Empezamos a definir la clase cuenta que vamos a utilizar

--- Clases/Ejercicio3.py
+++ Clases/Ejercicio3.py
@@ -4,3 +4,9 @@
 #Crea una cuenta bancaria a Plazo Fijo, en la cual cuando se retira dinero de algún modo antes de una Fecha de Vencimiento además del dinero a retirar se penaliza con un 5% adicional.
 #Crea además una cuenta Vip, que tendrá un atributo adicional que es el saldo negativo máximo que puede tener. En las cuentas Vip uno podrá tener saldo negativo siempre que no supere este valor.
 #A continuación, construye una aplicación que permita crear los tres tipos de cuentas. El ID tiene que ser un número entero incremental, el nombre del titular puede ser inventado, la fecha de apertura y fecha de vencimiento deben ser aleatorias siendo la fecha de apertura más antigua que la fecha de vencimiento, y el número de cuenta tiene que ser un número aleatorio de 12 dígitos. Cuando las cuentas estén iniciadas a un sueldo inicial de 10.000 €, transferir dinero de unas a otras las cantidades de 2000 €, ingresar 575 € y retirar dinero 78 €. 
+
+
+#Definimos la clase a utilizar
+
+class cuenta:
+    def __init__(self, cuenta, propietario, estado, saldo):
