15feb9afcc0d3803f01c041820fd1274b19bb57f
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-03-24 08:57:31 +0100

    Enunciado ejercicio 2

---
+++ Clases/Ejercicio2.py
@@ -0,0 +1,5 @@
+#Enunciado:
+#Considere los objetos siguientes: Animal, Mamífero, Ovíparo, Pollo, Gato, Ornitorrinco
+#Describa las relaciones entre los diferentes objetos. El diagrama asociado se llama diagrama de clases.
+#Dar un ejemplo de descripción algorítmica de las clases asociadas (únicamente la declaración de clase XXX).
+#¿Es posible implementar estos objetos en Python?
