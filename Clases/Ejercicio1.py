d1adf7045611a323ea6580fb5a5dd82342a02d1c
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-03-24 08:53:42 +0100

    Primer ejercicio

---
+++ Clases/Ejercicio1.py
@@ -0,0 +1,12 @@
+#Enunciado:Escriba una clase que permita describir un libro y situar los valores asociados. Dar un ejemplo de uso en Python.
+
+class libro:
+    def __init__ (nombre, autor, genero, añopublicacion, titulo, ISBN, edicion):
+        self.nombre = nombre libro
+        self.autor = autor
+        self.genero = genero
+        self.añopublicacion = año de publicacion
+        self.titulo = titulo
+        self.ISBN = ISBN
+        self.edicion = edicion
+print("libro")
